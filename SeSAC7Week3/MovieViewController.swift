aa4e07d0c5ae4262c402fcaaf3e74f3a54f1daa9
kyhlsd <kmyghn@gmail.com>
2025-07-11 12:15:13 +0900

    MovieTableViewController MovieViewController로 변경

---
+++ SeSAC7Week3/MovieViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewController.swift
+//  SeSAC7Week3
+//
+//  Created by 김영훈 on 7/11/25.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
